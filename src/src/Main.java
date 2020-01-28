b0419855eb0fb28641d8c451f24262a099466dcb
Vitalii Solodovnyk <vitaliysecretua@gmail.com>
2020-01-28 13:45:12 +0200

    add Car

---
+++ src/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Car car = new Car();
+        car.engine();
+        car.steeringWheel.turn();
+        car.body.paint();
+        car.wheel.setDiameter();
+        car.engineOff();
+
+
+    }
+}
---
+++ src/src/Wheel.java
@@ -0,0 +1,13 @@
+public class Wheel {
+
+    private int diameter;
+
+    public Wheel(int diameter) {
+        this.diameter = diameter;
+    }
+
+    public void setDiameter(){
+        System.out.println("Діаметр колеса " + diameter);
+    }
+
+}
